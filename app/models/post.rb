f42917bba6ed806bc6ce5366ebd5aefe4942940e
Hana Wang <h.wang081@gmail.com>
2014-02-19 09:54:47 -0600

    Add parsed_body method to Post and use it in view

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,16 @@ class Post < ActiveRecord::Base
 
   validates_presence_of :title
   validates_presence_of :body
+
+  def parsed_body
+    body = self.body
+    name = self.body.split('[').last.split(']').first
+    user = User.where(name: name).first
+    if user.present?
+      user_tag = "<a href='/users/#{user.id}'>#{name}</a>"
+      body = body.gsub("[#{name}]", user_tag)
+      body = body.html_safe
+    end
+    body
+  end
 end
