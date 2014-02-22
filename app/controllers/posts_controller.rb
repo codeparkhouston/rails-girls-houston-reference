f0778f8eb0ea89f323b461a34886629e5b6aecb2
Hana Wang <h.wang081@gmail.com>
2014-02-22 07:13:49 -0600

    Save and display geolocation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(params[:post])
+    @post.location = Geocoder.search(request.remote_ip).first.city
     if @post.save
       redirect_to user_path(@post.user)
     else
